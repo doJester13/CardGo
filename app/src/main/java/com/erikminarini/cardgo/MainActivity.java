2a59b65497ebefa114b597a29741903c2934c7c6
Erik Minarini <minarini.work@gmail.com>
2016-03-21 19:56:04 +0100

    base application

---
+++ app/src/main/java/com/erikminarini/cardgo/MainActivity.java
@@ -0,0 +1,98 @@
+package com.erikminarini.cardgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    AsyncHttpClient client = new AsyncHttpClient();
+    String TRIGGER = "http://10.5.5.9/gp/gpControl/command/shutter?p=1";
+    String STOP = " http://10.5.5.9/gp/gpControl/command/shutter?p=0";
+
+    Button trig;
+    Button stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trig = (Button) findViewById(R.id.button);
+        trig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request(TRIGGER);
+            }
+        });
+
+        stop = (Button) findViewById(R.id.button2);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request(STOP);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void request(String target){
+        client.get(target, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                // called when response HTTP status is "200 OK"
+
+                Log.v("STATUS",String.valueOf(statusCode));
+                Log.v("RESPONSE",responseBody.toString());
+                Log.v("CASE","SUCCESS");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.v("CASE","FAILURE");
+                Log.v("STATUS",String.valueOf(statusCode));
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+    }
+}
